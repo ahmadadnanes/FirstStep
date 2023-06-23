02130a9fb6069f8f97953fe9e50fa05fddb77a51
ahmad adnan <aa264048@gmail.com>
2023-06-23 21:23:40 +0300

    first-commit

---
+++ Home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- main css file -->
+    <link rel="stylesheet" href="css/Home.css">
+    <!-- others css files -->
+    <link rel="stylesheet" href="css/normal.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/brands.min.css">
+    <title>First Step</title>
+</head>
+
+<body bgcolor="#DCDCDC">
+    <!-- start header -->
+    <nav>
+        <div class="container">
+            <img src="img/logo-removebg-preview.png" width="75px">
+            <div class="links">
+                <span class="icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+
+                <ul>
+                    <li>
+                        <a href="#services">Servecis</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+
+
+    </nav>
+    <!-- end header -->
+</body>
+
+</html>
