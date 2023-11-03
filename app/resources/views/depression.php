547ffcef35a37e385d654c15de5b8df4c4e7f2e6
ahmad adnan <120600994+ahmadadnanes@users.noreply.github.com>
2023-11-03 18:01:41 +0300

    fix some problems

--- app/resources/views/depression.php
+++ app/resources/views/depression.php
@@ -10,7 +10,6 @@ $user = $_SESSION["user"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- main css files -->
     <?php include("./app/resources/components/layout.php") ?>
-    <link rel="stylesheet" href="/app/resources/css/DEPERSSION TEST.css">
     <title>Depression Test</title>
 </head>
 
